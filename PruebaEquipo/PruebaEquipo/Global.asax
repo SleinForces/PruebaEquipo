cf45a59fe021ea744cdb5594ca37d4603b69ce26
SleinForces <ultimatum.of.wisdom@live.com>
2016-06-13 00:40:22 -0500

    Proyecto Prueba

---
+++ PruebaEquipo/PruebaEquipo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PruebaEquipo.MvcApplication" Language="C#" %>
---
+++ PruebaEquipo/PruebaEquipo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PruebaEquipo.Startup))]
+namespace PruebaEquipo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
